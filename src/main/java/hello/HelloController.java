0280b853c19e24a667369ddfd86e2a29ef4d0bbd
tomnguyen592 <46578262+tomnguyen592@users.noreply.github.com>
2020-01-22 19:07:19 -0800

    tn - Fixed link

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
     public String index() {
         String html = "<h1>Hello World!</h1>\n" +
         "<p>This web app is powered by \n" +
-        "<a href='https://github.com/ucsb-cs56-w20/lab02-tomnguyen592'>this github repo</a></p>\n";
+        "<a href='https://github.com/ucsb-cs56-w20/lab02_tomnguyen592'>this github repo</a></p>\n";
         return html;
     }
     
